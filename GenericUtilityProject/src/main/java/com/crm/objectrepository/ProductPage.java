44683a85b80e4055f49c27f535b926f04fb5553a
rashm <rashm@SHRIRAM>
2024-12-27 10:11:43 +0530

    Product page added with create product link

---
+++ GenericUtilityProject/src/main/java/com/crm/objectrepository/ProductPage.java
@@ -0,0 +1,27 @@
+package com.crm.objectrepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage {
+	
+	WebDriver driver;
+	public ProductPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+			
+	}
+	
+	
+	
+	@FindBy(xpath="//img[@title='Create Product...']") 
+	private WebElement createproductbtn;
+	
+	public WebElement getCreateproductbtn() {
+		return createproductbtn;
+	}
+
+}
+
